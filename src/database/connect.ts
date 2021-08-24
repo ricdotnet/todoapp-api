7c3c5f916947ff7da17ba946ceb68527bf33ecde
Ricardo Rocha <me@rrocha.uk>
2021-08-24 21:51:05 +0100

    new database connection class;

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -1,13 +1,41 @@
-import mysql, {Connection} from 'mysql'
+import mysql, { Connection, Pool } from 'mysql';
 
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from 'dotenv';
+dotenv.config();
 
-export let connection: Connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: 3306,
-  connectTimeout: 5000,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
-})
+export class DbConnect {
+
+  private host: string = <string>process.env.DB_HOST;
+  private port: number = 3306;
+  private timeout: number = 5000;
+
+  private user: string = <string>process.env.DB_USER;
+  private password: string = <string>process.env.DB_PASSWORD;
+  private database: string = <string>process.env.DB_DATABASE;
+
+  createPool(): Pool {
+    return mysql.createPool({
+      host: this.host,
+      port: this.port,
+      timeout: this.timeout,
+      user: this.user,
+      password: this.password,
+      database: this.database
+    });
+  }
+
+  async getConnection() {
+    // return new Promise((resolve, reject) => {
+      this.createPool().getConnection((err, conn) => {
+        if (err) {
+          console.log('Could not connect.');
+          return;
+        }
+
+        // resolve(conn);
+        return conn;
+      });
+    // })
+  }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,9 @@ import dotenv from 'dotenv';
 
 import router from './routes';
 
-import { connection } from './database/connect';
+// import { connection } from './database/connect';
+import { DbConnect } from './database/connect';
+const db = new DbConnect();
 
 // create express app
 const app: express.Express = express();
@@ -22,19 +24,33 @@ app.use(morgan('dev'));
 app.use(router);
 
 // start the app/server
-const PORT: string = process.env.PORT || '4000'
+const PORT: string = process.env.PORT || '4000';
 app.listen(PORT, async () => {
 
-  try {
-    await connection.connect((error) => {
-      if (error)
-        return console.log(error);
-
-      console.log('Database connected!');
-      console.log(`Server is online and listening on port: ${PORT || 4000}`);
-      console.log(`\n==========\nhttp://localhost:${PORT || 4000}\n==========\n`)
-    });
+  try{
+    await db.getConnection().then(() => {
+      console.log('\n------------------------------\n'
+      + 'DATABASE CONNECTED\n'
+      + '------------------------------')
+    }).finally(() => {
+      console.log(`Server is listening on port ${PORT || 4000}`)
+    })
+    
+    // console.log(`Server is on and listening on port ${PORT || 4000}`)
   } catch (e) {
-    console.log(e);
+    console.log(e)
   }
+
+  // try {
+  //   await db.doConnect().connect((error) => {
+  //     if (error)
+  //       return console.log(error);
+
+  //     console.log('Database connected!');
+  //     console.log(`Server is online and listening on port: ${PORT || 4000}`);
+  //     console.log(`\n==========\nhttp://localhost:${PORT || 4000}\n==========\n`);
+  //   });
+  // } catch (e) {
+  //   console.log(e);
+  // }
 });
